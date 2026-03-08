6b56b0f87b64259e537cbb598bbfd1ba92c8efa8
mfaour <92956173+Agent-6@users.noreply.github.com>
2026-03-08 00:01:39 +0000

    Add Authentication service with initial project structure and configuration

---
+++ src/backend/src/Services/Authentication/ShopIt.Authentication.API/Program.cs
@@ -0,0 +1,43 @@
+using ShopIt.Framework.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.AddServiceDefaults();
+
+// request handlers
+builder.Services.AddApplicationServices(typeof(Program).Assembly);
+
+//builder.Services.AddPersistence("auth-db", builder.Configuration, typeof(Program).Assembly);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapDefaultEndpoints();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
