5ab84262f68cf8e3eeb911dd87aa3ca32166a2d3
mfaour <92956173+Agent-6@users.noreply.github.com>
2026-04-06 00:26:27 +0300

    Update the app component, add routing.

--- src/frontend/projects/portal/src/app/app.routes.ts
+++ src/frontend/projects/portal/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UsersPageComponent } from './users/users-page.component';
+import { UserDetailComponent } from './users/user-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: UsersPageComponent },
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: '**', redirectTo: 'users' }
+];
