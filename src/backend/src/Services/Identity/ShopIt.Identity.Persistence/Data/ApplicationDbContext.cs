bf023e3c72ebb99bcda8ef59a12804ca4dc80fa5
mfaour <92956173+Agent-6@users.noreply.github.com>
2026-04-19 16:46:27 +0300

    Configure DbContext for global tenant filtering.

--- src/backend/src/Services/Identity/ShopIt.Identity.Persistence/Data/ApplicationDbContext.cs
+++ src/backend/src/Services/Identity/ShopIt.Identity.Persistence/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ShopIt.Identity.Domain.Entities;
+using ShopIt.Identity.Domain.Tenancy;
 
 namespace ShopIt.Identity.Persistence.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, ICurrentTenant currentTenant) : IdentityDbContext<
     User,
     Role,
     Guid,
@@ -15,9 +17,38 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     UserToken
 >(options)
 {
+    private readonly ICurrentTenant _currentTenant = currentTenant;
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        ApplyTenantConfiguration(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    private void ApplyTenantConfiguration(ModelBuilder builder)
+    {
+        var applyTenantFilterMethod = typeof(ApplicationDbContext)
+            .GetMethod(nameof(ApplyTenantFilter), BindingFlags.NonPublic | BindingFlags.Instance);
+        
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            if (typeof(ITenantEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                applyTenantFilterMethod?.MakeGenericMethod(entityType.ClrType)
+                    .Invoke(this, [builder]);
+            }
+        }
+    }
+
+    private void ApplyTenantFilter<TEntity>(ModelBuilder builder)
+        where TEntity : class, ITenantEntity
+    {
+        builder.Entity<TEntity>()
+            .HasIndex(e => e.TenantId);
+
+        builder.Entity<TEntity>()
+            .HasQueryFilter(e => _currentTenant.Id == Guid.Empty || e.TenantId == _currentTenant.Id);
     }
 }
--- src/backend/src/Services/Identity/ShopIt.Identity.Persistence/Data/DesignTimeDbContextFactory.cs
+++ src/backend/src/Services/Identity/ShopIt.Identity.Persistence/Data/DesignTimeDbContextFactory.cs
@@ -1,16 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using ShopIt.Identity.Domain.Tenancy;
 
 namespace ShopIt.Identity.Persistence.Data;
 
-public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+public class DesignTimeDbContextFactory(ICurrentTenant currentTenant) : IDesignTimeDbContextFactory<ApplicationDbContext>
 {
+    private readonly ICurrentTenant _currentTenant = currentTenant;
+    
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         // Use a dummy string; only used by the CLI to generate migration files
         optionsBuilder.UseNpgsql("Host=localhost;Database=identity-db;Username=postgres;Password=postgres");
 
-        return new ApplicationDbContext(optionsBuilder.Options);
+        return new ApplicationDbContext(optionsBuilder.Options, _currentTenant);
     }
 }
